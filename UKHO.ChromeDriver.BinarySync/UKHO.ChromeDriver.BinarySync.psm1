46373c9324786a5b526bbf9998e6a9d7e4cda4b8
Daniel Gee <daniel.gee@ukho.gov.uk>
2019-05-28 14:14:12 +0100

    Swap psm1 to use Import-Module rather than dot sourcing

--- UKHO.ChromeDriver.BinarySync/UKHO.ChromeDriver.BinarySync.psm1
+++ UKHO.ChromeDriver.BinarySync/UKHO.ChromeDriver.BinarySync.psm1
@@ -1,7 +1,7 @@
 # load all internal files
 Get-ChildItem $PSScriptRoot/Internal -File -Recurse | ForEach-Object {
-    . $_.FullName
+    Import-Module $_.FullName
 }# load all script files
 Get-ChildItem $PSScriptRoot/Functions -File -Recurse | ForEach-Object {
-    . $_.FullName
+    Import-Module $_.FullName
 }
