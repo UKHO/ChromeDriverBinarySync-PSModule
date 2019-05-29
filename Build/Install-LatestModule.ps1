d136ba3b6bc9faeaa1ef7a161490b568924e3df2
Daniel Gee <daniel.gee@ukho.gov.uk>
2019-05-29 15:21:21 +0100

    Fix version check in Install-LatestModule

--- Build/Install-LatestModule.ps1
+++ Build/Install-LatestModule.ps1
@@ -4,7 +4,7 @@ param(
     $Name
 )
 $latestModule = Get-Module $Name | Sort-Object Version -Descending | Select-Object -First 1
-If ((Get-Module Pester -ListAvailable | Sort-Object Version -Descending | Select-Object -First 1).Version -lt $latestModule.Version) {
+If ((Get-Module $Name -ListAvailable | Sort-Object Version -Descending | Select-Object -First 1).Version -lt $latestModule.Version) {
     Install-Module -Name $Name -Scope CurrentUser -Force -Repository $latestModule.Repository
 }
 Import-Module -Name $Name
