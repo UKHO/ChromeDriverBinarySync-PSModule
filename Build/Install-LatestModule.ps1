cddfa52b5a33314c1c07e1b5fb5d6bf8f3e1ec10
Daniel Gee <daniel.gee@ukho.gov.uk>
2019-05-30 09:46:38 +0100

    Change Install-LatestModule to correctly find latest available and latest installed

--- Build/Install-LatestModule.ps1
+++ Build/Install-LatestModule.ps1
@@ -3,8 +3,11 @@ param(
     [string]
     $Name
 )
-$latestModule = Get-Module $Name | Sort-Object Version -Descending | Select-Object -First 1
-If ((Get-Module $Name -ListAvailable | Sort-Object Version -Descending | Select-Object -First 1).Version -lt $latestModule.Version) {
-    Install-Module -Name $Name -Scope CurrentUser -Force -Repository $latestModule.Repository
+$latestModule = Find-Module $Name -AllVersions | Sort-Object Version -Descending | Select-Object -First 1
+$installedModule = Get-Module -ListAvailable $Name | Sort-Object Version -Descending | Select-Object -First 1
+If ($installedModule.Version -lt $latestModule.Version) {
+    Write-Output "Installing $Name $($latestModule.Version)"
+    Install-Module -Name $Name -Scope CurrentUser -Force -Repository $latestModule.Repository -RequiredVersion $latestModule.Version
 }
+Write-Output "Importing $Name"
 Import-Module -Name $Name
