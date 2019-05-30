56a4ec7ddaef4139bea52e980a13fd4cc86ed78b
Daniel Gee <dgee2@users.noreply.github.com>
2019-05-30 17:07:00 +0100

    Replace Install-LatestModule with working version (#4)
    * Replace Install-LatestModule with working version
    
    Copied from ChromeDriver sync release
    
    * Fix pipeline to set PublishLocation if repository already exists

--- Build/Install-LatestModule.ps1
+++ Build/Install-LatestModule.ps1
@@ -1,13 +1,30 @@
 param(
     [Parameter(Mandatory)]
     [string]
-    $Name
+    $Name,
+    [Parameter(Mandatory)]
+    [string]
+    $RepositoryName,
+    [Parameter(Mandatory)]
+    [string]
+    $RepositorySourceUri
 )
+if (((Get-PSRepository -Name $RepositoryName -ErrorAction Ignore) | Measure-Object).Count -eq 0) {
+    Write-Output "Register $RepositoryName PSRepository"
+    Register-PSRepository -Name $RepositoryName -SourceLocation $RepositorySourceUri -InstallationPolicy Trusted
+    Write-Output "$RepositoryName PSRepository Registered"
+}
+Write-Output "Finding latest module"
 $latestModule = Find-Module $Name -AllVersions | Sort-Object Version -Descending | Select-Object -First 1
+Write-Output "latestModule: $($latestModule.Version)"
+Write-Output "Getting installed module"
 $installedModule = Get-Module -ListAvailable $Name | Sort-Object Version -Descending | Select-Object -First 1
+Write-Output "installedModule: $($installedModule.Version)"
 If ($installedModule.Version -lt $latestModule.Version) {
     Write-Output "Installing $Name $($latestModule.Version)"
     Install-Module -Name $Name -Scope CurrentUser -Force -Repository $latestModule.Repository -RequiredVersion $latestModule.Version
+    Write-Output "Installed $Name $($latestModule.Version)"
 }
 Write-Output "Importing $Name"
 Import-Module -Name $Name
+Write-Output "Imported $Name"
--- Build/Invoke-PSScriptAnalyzer.ps1
+++ Build/Invoke-PSScriptAnalyzer.ps1
@@ -1,5 +1,14 @@
-. $PSScriptRoot\Install-LatestModule.ps1 -Name PSScriptAnalyzer
-. $PSScriptRoot\Install-LatestModule.ps1 -Name Pester
+param(
+    [Parameter(Mandatory)]
+    [string]
+    $RepositoryName,
+    [Parameter(Mandatory)]
+    [string]
+    $RepositorySourceUri
+)
+
+. $PSScriptRoot\Install-LatestModule.ps1 -Name PSScriptAnalyzer -RepositoryName $RepositoryName -RepositorySourceUri $RepositorySourceUri
+. $PSScriptRoot\Install-LatestModule.ps1 -Name Pester -RepositoryName $RepositoryName -RepositorySourceUri $RepositorySourceUri
 
 $parameters = @{
     OutputFile = "$PSScriptRoot\Test-PSScriptAnalyzer.XML";
